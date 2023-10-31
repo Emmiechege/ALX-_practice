08355376476b003aeea74df58c3f8cb49a704079
Emmiechege <emilywanjachege@gmail.com>
2023-10-31 08:04:54 -0700

    appending data into a file

---
+++ 0x01-File_handling_c/0-file_append.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - appends data into a file
+ */
+void main(void)
+{
+	char abc.txt[50]; /* initialize the old file */
+	char dataToAppend[5]; /*size of data to append*/
+	FILE *fp = NULL; /* declare the file pointer*/
+
+	fp = fopen("abc.txt", "a"); /* open the file*/
+
+	if (fp == NULL) /* error handling*/
+	{
+		printf("Error");
+		exit(1);
+	}
+
+	/* open the file you want to append*/
+	printf("Enter the name of file: ");
+	scanf("%s", abc.txt);
+
+	/* write the data to append*/
+	printf("\nEnter data to append: ");
+	fgets(dataToAppend, 5, stdin); /*reads the inputted data to append*/
+
+	fputs(dataToAppend, fp); /*appends data to file*/
+
+	/* reopen file in read mode to print file contents*/
+	fp = freopen(abc.txt, "r", fp);
+
+	/*print file contents after appending */
+	printf("\nSuccesfully appended");
+
+	fclose(fp);
+}
+
+
