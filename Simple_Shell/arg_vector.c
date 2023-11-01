d36be5f6064f5cecae89af4ea4496d7033d71402
Emmiechege <emilywanjachege@gmail.com>
2023-11-01 03:27:47 -0700

    a program that prints all the arguments without using argc

---
+++ Simple_Shell/arg_vector.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints all the  arguments without using argc
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	int args = 0;
+
+	if
+	{
+		**argv != NULL;
+		printf("argv[args]", args);
+
+	}
+	return (0);
+}
