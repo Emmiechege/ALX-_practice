7f59204f23b7b82e914236ab6aa5878b231983ea
Emmiechege <emilywanjachege@gmail.com>
2023-11-02 05:03:43 -0700

    Aprogram that prints all command line arguments without using Argc

--- Simple_Shell/arg_vector.c
+++ Simple_Shell/arg_vector.c
@@ -9,8 +9,10 @@ int main(int argc, char **argv)
 {
 	int args = 1;
 
+	/*iterate through the array until you encounter a null pointer*/
 	while (argv[args] != NULL)
 	{
+		/* print and display value of the argument and the string*/
 		printf("Argv is %d: %s\n", args, argv[args]);
 		args++;
 	}
