b54f839d203f04eec70b91752d2677cbdda6c96a
Emmiechege <emilywanjachege@gmail.com>
2023-11-02 06:46:11 -0700

    A program that calculates the sum of command line arguments

---
+++ command_line_args/sum_of_integers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - calculates sum of command line arguments
+ * @argc: argument count
+ * @argv: argument vector
+ * Return - 0
+ */
+int main(int argc, char *argv[])
+{
+	int args_sum;/* a variable to hold the sum of args*/
+	int args; /*the loop counter*/
+
+
+	if (args > 0)
+	{
+		argc(args)++;
+	}
+	args_sum = strlen(argc);
+
+	return (0);
+}
