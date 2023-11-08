a443c726ef6dfbe2754bc94fd03de2b085e6324d
Emmiechege <emilywanjachege@gmail.com>
2023-11-08 00:58:07 -0800

    A program that tokenizes a string and prints all the tokens

--- Simple_Shell/strtok_02.c
+++ Simple_Shell/strtok_02.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <string.h>
-
+/**
+ * main - tokenizes a string and prints all the tokens
+ * Return: 0
+ */
 int main(void)
 {
 	char str[] = "I,love,coding,so,much";/*the string to be tokenized*/
 	char *delim = ",";/*the delimiter*/
-	*token;
+	char *token;
 
 	/*for the fitst string srtok is called with the input string*/
 	token = strtok(str, delim);
@@ -14,7 +17,7 @@ int main(void)
 	{
 		printf("Token is: %s\n", token);
 		/*for subsequent strings,str is called with NULL*/
-		token(NULL, delim);
+		token = strtok(NULL, delim);
 	}
 
 	return (0);
