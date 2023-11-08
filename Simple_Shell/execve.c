5668841bd094266576674843c06d6d4ad3f9e5ff
Emmiechege <emilywanjachege@gmail.com>
2023-11-08 04:49:10 -0800

    A program that replaces the current process with a new process

---
+++ Simple_Shell/execve.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - used to replace current process with a new process
+ * Return: 0
+ */
+int main(void)
+{
+	/* the first argument is file path, last argument is NULL pointer*/
+	char *argv[] = {"/bin/ls", "-l", NULL};
+	/*introduce a variable to hold the return value of execve*/
+	/*use the array argv[0], to access the filename*/
+	int val = execve(argv[0], argv, NULL);
+
+	if (val == -1)
+	{
+		perror("Error");
+	}
+	printf("Done with execve");
+
+	Return (0);
+}
