ab06545be5268731a760b3bd790312c50981fbe4
Emmiechege <emilywanjachege@gmail.com>
2023-11-08 01:56:38 -0800

    A program that tokenizes a string,allocates memory to it and prints the tokens

---
+++ Simple_Shell/strtok_03.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * main - parses and tokenizes a string, allocates memory and prints the tokens
+ * Return: 0
+ */
+int main(void)
+{
+	char *src = "coding makes me so happy";
+	/*str holds a copy of the string, allocate memory for it*/
+	/*the length of memoty ro be allocate is determined by the length of src*/
+	char *str = malloc(sizeof(char) * strlen(src));
+	char *delim = " ";/*the delimiter*/
+	char *token;/*holds the tokens*/
+
+	strcpy(str, src);/*copy contents of src into str*/
+
+	token = strtok(str, delim);
+
+	while(token != NULL)
+	{
+		printf("%s\n", token);
+		token = strtok(NULL, delim);
+	}
+
+	return (0);
+}
+
