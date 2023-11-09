ff573dcd8d7fc4ad8cee122547e3d19ce1797f52
Emmiechege <emilywanjachege@gmail.com>
2023-11-09 05:02:18 -0800

    A program that creates a new process by duplicating the calling process

--- simple_shell_prac/fork.c
+++ simple_shell_prac/fork.c
@@ -12,18 +12,21 @@ int main(void)
 	/*invoke fork system call*/
 	child_pid = fork();/*child process is created here*/
 
-	if (child_pid == -1) /* meand the system call failed*/
+	if (child_pid == -1) /* means the system call failed*/
 	{
 		perror("Error");
 	}
 	if (child_pid == 0)
 	{
+	/* child_pid has process ID of newly created child  process*/
+	/*if child_pid is 0,it means we are inside the child process*/
 	/* this code is executed by the child process */
 		printf("I am the child process\n");
 	}
 	else
 	{
 	/*this code is executed by the parent process*/
+	/*if child_pid is greater than 0, it means we are in the parent process*/
 		printf("I am the parent process\n");
 	}
 
