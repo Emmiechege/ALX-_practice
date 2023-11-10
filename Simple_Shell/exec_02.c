2600924082cf8bfdf51f312fd86638cc10eea623
Emmiechege <emilywanjachege@gmail.com>
2023-11-10 06:19:25 -0800

    a program that replaces the current process with a new process and creates a child process

---
+++ Simple_Shell/exec_02.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+/**
+ * main - executes programs and creates a child process
+ * Return: 0
+ */
+int main(void)
+{
+	pid_t child_pid;
+
+	char *argv = {"/bin/ls", "-l", NULL};
+
+	/*invoke the fork system call to create child process*/
+		child_pid = fork();
+
+		/*check if fork system call fails*/
+		if (child_pid == -1)
+		{
+			perror( "Error");
+		}
+		if (Child_pid == 0)
+		{
+		/*the child process*/
+		/*variable to hold return value of exec*/
+			int val = execve(argv[0], argv, NULL);
+
+			if (val == -1)/*check if exec system call fails*/
+			{
+				perror("Error");
+			}
+		}
+		else
+		{
+			/*parent process*/
+			/* wait is invoked in the parent process*/
+			wait(NULL);
+			printf("Just executed\n");
+		}
+		return (0);
+}
