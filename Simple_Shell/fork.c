bddb22c8efa2e9a00cd4df7b250d849922d5235c
Emmiechege <emilywanjachege@gmail.com>
2023-11-09 23:04:04 -0800

    A program that creates a process similar to the parent process

---
+++ Simple_Shell/fork.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - creates a new process similar to the parent process
+ * Return: 0
+ */
+int main(void)
+{
+	/*a variable to hold the return value of the child process*/
+	pid_t child_pid;
+
+	if (child_pid == -1)
+	{
+	/* check if system call ws succesful*/
+		perror("Error");
+	}
+	
+	child_pid = fork();/*invoke fork to create the child process*/
+
+	if (child_pid > 0)
+	{
+		printf("I am the parent process");
+	}
+	else (child == 0)
+	{
+		printf("i am the child process");
+	}
+
+	return (0);
+}
