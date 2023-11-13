d8fd875477a72b1e0a2878bb6172d6e95b0a588b
Emmiechege <emilywanjachege@gmail.com>
2023-11-13 12:33:37 -0800

    A program that checks existence of files specified as command line arguments and prints whether each file is found or not

--- Simple_Shell/stat_02.c
+++ Simple_Shell/stat_02.c
@@ -16,3 +16,19 @@ int main (int ac, char **v)
 		printf ("Usage: %s path_to_file ...\n", av[0]);
 		return (1);
 	}
+	i = 1;
+	while (av[i])
+	{
+		printf("%s:", av[i]);
+		if (stat(av[i], &st) == 0)
+		{
+			printf(" FOUND\n");
+		}
+		else
+		{
+			printf(" NOT FOUND\n");
+		}
+		i++;
+	}
+	return (0);
+}
