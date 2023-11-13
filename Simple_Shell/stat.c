5ede4b4d3a967b9081f5eaf6f03206f1dc4618cb
Emmiechege <emilywanjachege@gmail.com>
2023-11-13 06:15:18 -0800

    A program used to obtain information about the filename of a given file

---
+++ Simple_Shell/stat.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/stat.h>
+/**
+ * main - used to obtain information about the file name
+ * Return: 0
+ */
+int main(void)
+{
+	const char *file_name;
+	struct stat buff;
+
+	if (stat(file_name, &buff) == 0)
+	{
+		printf("File Size: %lld bytes\n", (long long)buff.st_size);
+	}
+	else
+	{
+		perror("stat");
+	}
+
+	return (0);
+}
