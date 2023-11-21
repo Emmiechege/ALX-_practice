008a1c5b10c3ad8f647008e19a2c690e82b93e55
Emmiechege <emilywanjachege@gmail.com>
2023-11-20 21:22:26 -0800

    A program that creates a structure

--- Structures/structs.c
+++ Structures/structs.c
@@ -3,18 +3,19 @@
  * main - creates a structure
  * Return: 0
  */
-struct person {
+struct person
+{
 	char *name;
 	int age;
 	char school[20];
 };
-int main (void)
+
+int main(void)
 {
 	struct person person1;
 
 	person1.name = "Emmie";
 	person1.age = 22;
 	person1.school = "ALX";
-
-	Return (0);
+	Return(0);
 }
